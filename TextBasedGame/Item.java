62ecfb32b643631b19e6fbd7a7600edcf33d27be
conhuang <30940887+conhuang@users.noreply.github.com>
2018-03-01 10:05:52 -0800

    Add files via upload

---
+++ TextBasedGame/Item.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Item here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Item extends Entity
+{
+
+    public Item(String n)
+    {
+        super(n);
+        }
+    
+}
