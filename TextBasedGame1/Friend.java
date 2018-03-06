d82aabdd7c45b92828f47a633eb31e7b656fc708
conhuang <30940887+conhuang@users.noreply.github.com>
2018-03-06 00:09:56 -0800

    Add files via upload

---
+++ TextBasedGame1/Friend.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Friend here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Friend extends Person
+{
+    public Friend(String n, String d)
+    {
+        super(n,d);
+    }
+
+    public void build(Item i){
+        
+    }
+}
